dce5d2b248154f7b99350a0ee0db8f2426493b41
Ari Jordan <ari.jordan.ch@gmail.com>
2024-03-21 15:20:37 +0100

    Finish data upload script

--- setup_weaviate_index.py
+++ setup_weaviate_index.py
@@ -1,24 +1,60 @@
+import os
+import dotenv
+import weaviate
+from typing import List
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain.retrievers.weaviate_hybrid_search import WeaviateHybridSearchRetriever
-import weaviate
-from weaviate.embedded import EmbeddedOptions
-import dotenv
+from langchain_core.documents.base import Document
+from langchain_community.document_loaders import JSONLoader
+
 
 dotenv.load_dotenv()
+OPENAI_API_KEY_STARTHACK = os.getenv("OPENAI_API_KEY_STARTHACK")
+WEAVIATE_URL_STARTHACK = os.getenv("WEAVIATE_URL_STARTHACK")
 
-client = weaviate.Client(embedded_options=EmbeddedOptions())
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY_STARTHACK)
+
+# Connect to a WCS instance
+client = weaviate.Client(url=WEAVIATE_URL_STARTHACK)
 
 retriever = WeaviateHybridSearchRetriever(
     alpha=0.5,  # Weighting between keyword and semantic search
     client=client,
-    index_name="starthack-1noigzvf",  #  Sandbox index
+    index_name="Documents",
     text_key="text",
     attributes=[],  # No additional attributes for now
 )
 
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=250)
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(texts)
+file_documents = []
 
-embeddings = OpenAIEmbeddings()
+if False:
+    loader = JSONLoader(
+        file_path="data.json",  # TODO set things like: jq_schema=".messages[].content", text_content=False
+    )
+
+    data: List[Document] = loader.load()
+
+# Mock list of documents
+data: List[Document] = [
+    Document(
+        id="1",
+        page_content="This is a test document.",
+        text="This is a test document.",
+        # attributes={"title": "Test document"},
+    ),
+    Document(
+        id="2",
+        page_content="This is a test document.",
+        text="This is another test document.",
+        # attributes={"title": "Another test document"},
+    ),
+]
+
+# Split the text into chunks
+chunks: List[Document] = text_splitter.split_documents(data)
+
+retriever.add_documents(chunks)
+print("Upload completed.")
